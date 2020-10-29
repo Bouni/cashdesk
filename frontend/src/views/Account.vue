ab63ecd80168f404e9112d350eefdeecb5311621
Bouni <bouni@owee.de>
2020-10-29 07:33:57 +0100

    hide items that are not in stock

--- frontend/src/views/Account.vue
+++ frontend/src/views/Account.vue
@@ -63,22 +63,6 @@
                           </v-col>
                         </v-row>
                         </v-card>
-                        <v-card class="grey darken-2" v-else>
-                        <v-row dense>
-                          <v-col cols="12" sm="12" md="9">
-                            <v-card-title class="grey--text">
-                            {{ item.brand }}
-                            </v-card-title>
-                            <v-card-subtitle class="grey--text">
-                              {{ item.name }}<br/>
-                              {{ item.size }}<span class="ml-3">{{ item.price | euro }}</span>
-                            </v-card-subtitle>
-                          </v-col>
-                          <v-col cols="12" sm="12" md="3" class="my-auto">
-                              <v-img :src="item.image" max-height="7em" contain style="filter: grayscale(100%);"></v-img>
-                          </v-col>
-                        </v-row>
-                        </v-card>
                       </v-col>
 				    </v-row>	
 				    </v-container>
