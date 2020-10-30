f59aaae0f8d6e23cdb97608844c1a6684bc5b0e5
Bouni <bouni@owee.de>
2020-10-30 11:44:24 +0100

    removed dev artifact

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,6 +4,4 @@ python3 /app/manage.py makemigrations --noinput
 python3 /app/manage.py migrate --noinput
 python3 /app/manage.py collectstatic --noinput 
 
-ls -la /app
-
 exec gunicorn --bind :8000 --workers 3 cashdesk.wsgi
