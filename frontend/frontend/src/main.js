d0656ac3543ef76c06384e0f500b3cc1ba36ec51
Bouni <bouni@owee.de>
2020-11-03 12:46:52 +0100

    add UI to edit products

--- frontend/frontend/src/main.js
+++ frontend/frontend/src/main.js
@@ -3,8 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import VueCookies from "vue-cookies";
- 
+import Vuelidate from "vuelidate";
+
 Vue.use(VueCookies);
+Vue.use(Vuelidate);
 
 Vue.config.productionTip = false;
 
